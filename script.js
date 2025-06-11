51336468a34e4a6eafa4476c4dd391576319fd75
MoisesEastland1 <meastland977@gmail.com>
2025-06-11 17:39:59 -0500

    added notes to script file

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
+/*record the input from user */
+/*store input from user */
+/*underline input from user */
+/*delete under from user with a button */
 const form = document.getElementById('form');
 const input = document.getElementById('input');
 const todosUl = document.getElememtById('todos');
+
+const todos = JSON.parse(localStorage.getItem('todos'))
