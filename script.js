0f33f9b3ae3403fe0815f6f701afb93f6d826612
MoisesEastland1 <meastland977@gmail.com>
2025-06-30 20:09:46 -0500

    capture input value then add to array

--- script.js
+++ script.js
@@ -1,16 +1,42 @@
-/*record the input from user */
-/*store input from user */
-/*underline input from user */
-/*delete under from user with a button */
+//grabbed the form by id frm HTML
 const form = document.getElementById('form');
+//grabbed the input by id frm HTML
 const input = document.getElementById('input');
-//const todosUl = document.getElememtById('todo-entry');
+//grabbed the unorder-list(todo-list) by id frm HTML
+const todoList = document.getElementById('todo-list');
+
 
 //const todos = JSON.parse(localStorage.getItem('todos'))
 
-form.addEventListener('submit', (e) => {
-  e.preventDefault()
-  console.log('form submitted');
+//this the Todo Array initialized where input values will go
+let allTodosArr = [];
 
-  /* todo function here*/
+//this the Event Listener for submitting input values
+form.addEventListener('submit', (e) => {
+  //this preventing the default activity like page reload
+  e.preventDefault();
+
+  //This the add todo function at play
+  addTodo()
+  
+  
 })
+
+//this the inner workings of the add todo function
+function addTodo() {
+  // the input value is known as todoInputText w/ trimmed whitespace via trim method
+  const todoInputText = input.value.trim();
+
+  // preventing empty todos values frm being created 
+  if(todoInputText.length > 0) {
+
+    //pushing input value into the todo array
+  allTodosArr.push(todoInputText);
+
+  
+  //identifing and empty todo values
+  todoInputText.value = "";
+  }
+  
+  console.log(allTodosArr);
+  }
